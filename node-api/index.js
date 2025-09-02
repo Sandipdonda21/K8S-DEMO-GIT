2e378f4351b125370f42c10a6d0dd423b7312451
sandipdonda <sandipdonda2121@gmail.com>
2025-09-02 11:53:19 +0530

    missing dependancy: 'redis'

--- node-api/index.js
+++ node-api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sql = require('mssql');
 const app = express();
 const cors = require('cors');
+const redis = require('redis');
 
 app.use(cors());
 app.use(express.json());
